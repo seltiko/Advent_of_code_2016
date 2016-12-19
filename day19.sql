20e3c23a8d117ba948b4acbb8b9ecb2b18944238
seltiko <fost97@gmail.com>
2016-12-19 13:57:07 -0600

    Day19
    I don't know how I feel about this one, it's just the closed form solution, which might be against the spirit of advent.

---
+++ day19.sql
@@ -0,0 +1,22 @@
+﻿-- 3005290
+
+--Part I
+select 1 +
+	2 * 
+	(3005290 - (2^((floor(log(2,3005290)))::int))::int) as math
+;
+
+
+
+
+
+--Part II
+select case when 3005290 - 3^floor(log(3,3005290))::int + 
+	case when 3005290 - 2 * 3^floor(log(3,3005290))::int >= 0 
+		then 3005290 - 2 * 3^floor(log(3,3005290))::int
+		else 0 end
+		= 0 then 3005290 else
+	3005290 - 3^floor(log(3,3005290))::int + 
+	case when 3005290 - 2 * 3^floor(log(3,3005290))::int >= 0 
+		then 3005290 - 2 * 3^floor(log(3,3005290))::int
+		else 0 end end as math
